274b5e104f102497c34b6d811bae6911802065a6
sqr00t <797346+sqr00t@users.noreply.github.com>
2024-08-01 15:51:54 +0100

    refactor: update chains and retrievers

--- rag/vector_databases/__init__.py
+++ rag/vector_databases/__init__.py
@@ -1,5 +1,4 @@
 from asf_hp_installer_chatbot import db_path
 from rag.vector_databases.databases import init_vdb
-from rag.vector_databases.retrievers import chatbot_retriever
 
-__all__ = ["init_vdb", "db_path", "chatbot_retriever"]
+__all__ = ["db_path", "init_vdb"]
--- rag/vector_databases/retrievers.py
+++ rag/vector_databases/retrievers.py
@@ -1,14 +1,24 @@
+from langchain_core.vectorstores import VectorStoreRetriever
+
 from rag.vector_databases import init_vdb
 from rag.utils.configurables import (
     retriever_searchtype_configurable,
     retriever_searchkwargs_configurable,
 )
 
-chatbot_retriever = (
-    init_vdb()
-    .as_retriever()
-    .configurable_fields(
-        search_type=retriever_searchtype_configurable(),
-        search_kwargs=retriever_searchkwargs_configurable(),
+
+def init_chatbot_retriever(**kwargs) -> VectorStoreRetriever:
+    """
+    Initializes a retriever for the chatbot Qdrant vector document store, with search_type and search_kwargs as configurable fields.
+
+    Returns:
+        VectorStoreRetriever: Qdrant vector store retriever with search_type and search_kwargs configurable fields.
+    """
+    return (
+        init_vdb(**kwargs)
+        .as_retriever()
+        .configurable_fields(
+            search_type=retriever_searchtype_configurable(),
+            search_kwargs=retriever_searchkwargs_configurable(),
+        )
     )
-)
